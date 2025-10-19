aa79620f72b98b3362f32d9a3003a0ad7a5aa25f
divya123negi <divyanegi081466@gmail.com>
2025-10-20 02:07:37 +0530

    fixed

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+ baseURL:
+    import.meta.env.VITE_API_BASE_URL ||
+    (import.meta.env.MODE === "development"
+      ? "http://localhost:5000/api"
+      : "/api"),
 });
 
 // Get/Set token from storage
